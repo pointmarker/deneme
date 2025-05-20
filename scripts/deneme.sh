0e7170006b1fcd3a1a81f6b001be77150c48e370
pointmarker <yagizalanli@gmail.com>
2025-05-20 20:54:14 +0300

    feat: log tracking system added with json base

--- scripts/deneme.sh
+++ scripts/deneme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PSQL="psql -U postgres -d noteapp -A -t -c"
+history=("menu")
 menu(){
     if [[ $1 ]]
     then
@@ -9,60 +10,110 @@ menu(){
     echo -e "\n~~ Not Defteri ~~\n"
     echo -e "kullanıcı adı girin:"
     read username
-    if [[ -z "$username" ]]
-    then
-        pass=false
-        until [[ $pass == true ]]
-        do
-            echo -e "kullanıcı adı girin:"
-            read username
-            if [[ -n "$username" ]]
-            then
-                pass=true
-            fi
-        done
-    fi
+    while [[ -z "$username" ]]
+    do
+        echo -e "kullanıcı adı girin:"
+        read username
+    done
     user_req=$($PSQL "SELECT username FROM users WHERE username = '$username'" 2>> ./logs/err.log) 
     if [[ -z $user_req ]]
     then
-        echo -e "sen daha önce kayıt olmamışsın seni kaydediyorum\n"
+        echo -e "\nsen daha önce kayıt olmamışsın seni kaydediyorum\n"
         INSERT_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$username')")
         if [[ -n $INSERT_RESULT ]] 
         then
-            date="now"
+            local now=$(date)
             echo yes
-            log $username "creates $username on users at $date"
+            log $username "creates $username on users at $now"
         fi
     else
-        echo -e "hoş geldin "$username"!\n" 
+        echo -e "\nhoş geldin "$username"!\n" 
     fi
-    echo -e "1.Not yaz\n2.Notları gör\n3.Not sil\n4.Çıkış"
+    history+=("menu")
+    echo -e "1.Not yaz\n2.Notları gör\n3.Not sil\n4.Geri\n5.Çıkış\n"
     read select
     case $select in
         1)add_note;;
         2)see_notes;;
         3)delete_note;;
-        4)exit;;
+        4)go_back;;
+        5)exit;;
         *)menu geçersiz;;
     esac
 }
-log(){
-    if [[ $1 && $2 ]]
+go_back(){
+    if [[ ${#history[@]} -ge 2 ]]
     then
-        echo "$2" 1>> "./logs/$1.log"
+        func=${history[-2]}
+        unset 'history[-1]'
+        history=("${history[@]}")
+        $func
+    else
+        echo -e "geri gidecek yer yok"
+        sleep 2
+        menu
+    fi
+}
+log(){
+    local user=${1-}
+    local level=${2-}
+    local action=${3-}
+    local now = $(date)
+    local file=".logs/$user-log.json"
+
+    if [[ ! -f "$file" ]]
+    then
+        echo "[]" > "$file"
+    fi
+
+    head -c -1 "$file" > "${file}.tmp"
+
+    if [[ $(wc -c < "${file}.tmp") -le 2 ]]
+    then
+        echo -n "[{\"timestamp\":\"$now\",\"level\":\"$level\",\"action\":\"$action\",\"message\":\"$message\"}]" > "$file"
+    else 
+        echo -n ",{\"timestamp\":\"$now\",\"level\":\"$level\",\"action\":\"$action\",\"message\":\"$message\"}]" >> "${file}.tmp"
+        mv "${file}.tmp" "$file"
     fi
 }
 add_note(){
-    menu
+    history+=("add_note")
+    clear
+    echo -e "notunuzu ekleyin:"
+    read note
+    echo -e "\ngrubu var mı:"
+    read group
+    local now=$(date)
+    log $username "$now -- $group -- $note"
+    echo -e "başarılı!\n"
+    alternate_menu
 }
+
+alternate_menu(){
+    if [[ $1 ]]
+    then
+        echo -e "\n$1"
+    else
+    echo -e "1.Geri\n2.Menu\n3.Çıkış\n"
+    read answer
+    case $answer in
+        1)go_back;;
+        2)menu;;
+        3)exit;;
+        *)alternate_menu "nası yani??";;
+    esac
+    fi
+}
+
 see_notes(){
-    menu
+    history+=("see_notes")
+    alternate_menu
 }
 delete_note(){
-    menu
+    echo "delete"
 }
 exit(){
-    echo -e "end of the session of..."
+    echo -e "\ngörüşürüz $username"
 }
 
 menu
